4c45100829a1fc3e93615a5a7b41b96dba918473
johnmcha <johnnycha90@gmail.com>
2023-02-02 16:25:10 +1300

    feat: add some more interesting question into the code

--- numguess.py
+++ numguess.py
@@ -3,4 +3,10 @@ from random import randint
 # create random number integer
 answer = randint (1, 100)
 print(answer)
+# get user's name, guess
 
+user_name = input('hello what is your name?')
+guess = input(f'hi,{user_name}. guess the number (1-100): ')
+
+# print to check
+print(user_name, guess)
